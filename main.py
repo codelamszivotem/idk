c55c649ebfd39109edd829370666502e5bf2a007
codelamszivotem <144122024+codelamszivotem@users.noreply.github.com>
2023-09-05 13:24:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print "kys"
+Letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+Log = input("Insert your password: ")
+pass_let = 
+encryption =
+Output = print ("Your encrypted password: "+ encryption)
